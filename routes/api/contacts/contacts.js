e004d28dcbadc0c0b4dba52e2eaef9b4083bc7ca
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-06-19 13:22:48 +0200

    put

--- routes/api/contacts/contacts.js
+++ routes/api/contacts/contacts.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { contactsController } = require('../../../controller')
+const { contactsController } = require("../../../controller");
 const {
   validationAddContact,
   validationUpdateContact,
-} = require('./validate.js')
+} = require("./validate.js");
 
 router
-  .get('/', contactsController.getAll)
-  .get('/:contactId', contactsController.getById)
-  .post('/', validationAddContact, contactsController.add)
-  .delete('/:contactId', contactsController.remove)
-  .patch('/:contactId', validationUpdateContact, contactsController.update)
+  .get("/", contactsController.getAll)
+  .get("/:contactId", contactsController.getById)
+  .post("/", validationAddContact, contactsController.add)
+  .delete("/:contactId", contactsController.remove)
+  .put("/:contactId", validationUpdateContact, contactsController.update);
 
-module.exports = router
+module.exports = router;
